2babcb3cc7849f02f0c35403d5624cf51aa72aeb
Steve Engledow <steve.engledow@proxama.com>
2015-07-27 00:04:18 +0100

    Add room stub

---
+++ room/server.py
@@ -0,0 +1,51 @@
+from bottle import *
+
+@get("/rooms")
+def get_rooms(mongodb):
+    return {
+        "rooms": [
+            {
+                "id": "room id 1",
+                "name": "room name 1",
+                "title": "room title 1",
+                "members": ["user id 1", "user id 2"]
+            },
+            {
+                "id": "room id 2",
+                "name": "room name 2",
+                "title": "room title 2",
+                "members": ["user id 2"]
+            }
+        ]
+    }
+
+@post("/rooms")
+def create_room(mongodb):
+    return {
+        "id": "new room id",
+        "name": "new room name",
+        "title": "new room title",
+        "members": ["new user id"]
+    }
+
+@get("/users")
+def get_users(mongodb):
+    return {
+        "users": [
+            {
+                "id": "user id 1",
+                "name": "user name 1"
+            },
+            {
+                "id": "user id 2",
+                "name": "user name 2"
+            }
+        ]
+    }
+
+@post("/users")
+def create_user(mongodb):
+    return {
+        "id": "new user id",
+        "name": "new user name"
+    }
