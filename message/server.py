1aaacb00f0a514529f1114a9692e17d424870a1e
Steve Engledow <steve.engledow@proxama.com>
2015-07-27 00:03:48 +0100

    Add message stub

---
+++ message/server.py
@@ -0,0 +1,28 @@
+from bottle import *
+
+@get("/messages")
+def get_messages(mongodb):
+        "messages": [
+            {
+                "id": "message id 1",
+                "user": "user id 1",
+                "room": "room id 1",
+                "text": "message text 1"
+            },
+            {
+                "id": "message id 2",
+                "user": "user id 2",
+                "room": "room id 2",
+                "text": "message text 2"
+            }
+        ]
+    }
+
+@post("/messages")
+def create_message(mongodb):
+    return {
+        "id": "room id",
+        "user": "user id",
+        "room": "room id",
+        "text": "message text"
+    }
